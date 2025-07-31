af459b1fa39d2de3f542b7176e2b319bf8cc6547
Edward Anthony R. Quianzon <edward.quianzon.r@gmail.com>
2025-07-31 23:32:14 +0800

    Update app_routes.dart

--- lib/services/app_routes.dart
+++ lib/services/app_routes.dart
@@ -1,1 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:health_share/screens/home/home.dart';
+import 'package:health_share/screens/login/login.dart';
+import 'package:health_share/screens/profile/profile.dart';
+import 'package:health_share/screens/settings/settings.dart';
 
+final Map<String, WidgetBuilder> appRoutes = {
+  '/home': (context) => const HomeScreen(),
+  '/login': (context) => const LoginScreen(),
+  '/profile': (context) => const ProfileScreen(),
+  '/settings': (context) => const SettingsScreen(),
+};
