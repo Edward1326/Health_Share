81796ea234ab00414495661fa373681f3533ec26
Edward Anthony R. Quianzon <edward.quianzon.r@gmail.com>
2025-07-31 23:33:50 +0800

    Create settings.dart

---
+++ lib/screens/settings/settings.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SettingsScreen extends StatefulWidget {
+  const SettingsScreen({super.key});
+
+  @override
+  State<SettingsScreen> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<SettingsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
